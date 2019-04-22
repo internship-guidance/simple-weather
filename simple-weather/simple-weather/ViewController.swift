476462d8c9d96864948088445c7a086d3a3a104a
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-22 20:58:43 +0300

    Develop branch commit

---
+++ simple-weather/simple-weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-weather
+//
+//  Created by egmars.janis.timma on 22/04/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
