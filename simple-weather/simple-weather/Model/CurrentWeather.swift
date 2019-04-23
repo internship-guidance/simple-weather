62b158eab8a8a94d601f17d8b35a8021fceddf34
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-23 11:58:05 +0300

    Updated tableView location and bottom constraints

---
+++ simple-weather/simple-weather/Model/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  simple-weather
+//
+//  Created by egmars.janis.timma on 23/04/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+//import Foundation
+//
+//class CurrentWeather {
+//    
+//    private var _cityName: String!
+//    private var _date: String!
+//    private var _weatherType: String!
+//    private var _currentTemp: Double!
+//    
+//}
