2bcf5530c7da07c28426819a84a855e5224d231d
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-23 15:06:23 +0300

    Imported Corelocation framework to notice users current location coordinates

--- simple-weather/simple-weather/Controller/ViewController.swift
+++ simple-weather/simple-weather/Controller/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var locationLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+//    var currentWeather: CurrentWeather!
+//
+//    let url = "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=4ec61b9764720fc34bc6123d2169ab81"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+//        let stringURL = NSURL(string: url)
+//        let weatherObject = NSData(contentsOf: stringURL! as URL)
+//        print(weatherObject)
+//
+//        currentWeather = CurrentWeather()
+//        currentWeather.downloadCurrentWeather() {
+//            print("Data is downloaded!!!")
+//        }
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+//        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        locationLabel.text = "\(locValue.latitude) \(locValue.longitude)"
+    }
 }
 
